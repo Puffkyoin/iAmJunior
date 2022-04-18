6c04acf277f7a15bcf2ed9c2749e3f2136f60498
Puffkyoin <103963648+Puffkyoin@users.noreply.github.com>
2022-04-19 01:17:05 +0300

    Add files via upload

---
+++ task2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string enemyName = "Hobando";
+        var hitPoint = 100;
+        int attack = 10;
+        bool edeble = true;
+        uint enemyLevel = 10;
+        float enemySpeed = 4.5f;
+        byte enemyHurt = 1;
+        char firstLetterOfName = 'H';
+        decimal nutritionalValue = 122254.451m;
+    }
+        enum enemySkill
+    {
+        bitterSeads,
+        starchBomb,
+    }
+    enemySkill skill = enemySkill.starchBomb;
+}
+    

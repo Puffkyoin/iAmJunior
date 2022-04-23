9bc29b6879884673681dc694d1d6332ab8b8100d
Puffkyoin <103963648+Puffkyoin@users.noreply.github.com>
2022-04-24 00:46:46 +0300

    Add files via upload

---
+++ task5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 10;
+        int y = 20;
+        Console.WriteLine($"Старые значения:\n x = {x}\n y = {y}");
+        int temp = x;
+        x = y;
+        y = temp;
+        Console.WriteLine($"Новые значения:\n x = {x}\n y = {y}");
+    }
+}   

6756f7bdc24a6a73e4217fe8ad0612d2a92be013
Puffkyoin <103963648+Puffkyoin@users.noreply.github.com>
2022-04-23 23:36:17 +0300

    Add files via upload

---
+++ task2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string enemyName = "Hobando";
+        int hitPoint = 100;
+        byte enemyLevel = 10;
+        uint attack = 100;
+        float enemySpeed = 4.5f;
+        bool isEdeble = true;
+        ulong battleCounter = 10000;
+        decimal nutritionalValue = 122254.451m;
+        long bodyTemperature = 7;
+        object enemyWeakness = "low temperatures";
+    }
+}
+    

05f36501f3c48906d29f7ede0b25f45253f5f092
Puffkyoin <103963648+Puffkyoin@users.noreply.github.com>
2022-04-24 00:19:01 +0300

    Add files via upload

---
+++ task4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name;
+        string favoriteFood;
+        int age;
+        Console.WriteLine("Введите своё имя");
+        name = Console.ReadLine();
+        Console.WriteLine("Сколько тебе лет? (Введите число)");
+        age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите своё любимое блюдо.(в единственном числе)");
+        favoriteFood = Console.ReadLine();
+        Console.WriteLine($"Тебя зовут {name}. Твой возраст {age} и твоё любимое блюдо {favoriteFood}.");
+    }
+}
+    

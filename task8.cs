7c1f3ad994f8b5ede47c571e6d07a22c40f592c6
Puffkyoin <103963648+Puffkyoin@users.noreply.github.com>
2022-04-24 02:01:48 +0300

    Add files via upload

---
+++ task8.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Скалько Babuskas вы видите впереди?");
+        int babushkas = Convert.ToInt32(Console.ReadLine());
+        int waitTime = 10;
+        int totalHoursWait = babushkas * waitTime/60;
+        int totalMinutesWait = babushkas * waitTime % 60;
+        Console.WriteLine($"Вы должны отстоять в очереди {totalHoursWait} час(а) и {totalMinutesWait} минут(ы)");
+    }
+}   

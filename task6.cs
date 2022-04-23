3a736c3dc7d41088d253dcc9881863673cd18ba1
Puffkyoin <103963648+Puffkyoin@users.noreply.github.com>
2022-04-24 01:14:06 +0300

    Add files via upload

---
+++ task6.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int photosInAlbum = 52;
+        int photosInRow = 3;
+        int rowsFilled = photosInAlbum / photosInRow;
+        int remainder = photosInAlbum % photosInRow;
+        Console.WriteLine($" Заполнено {rowsFilled} строк.\n Остаток фото равен {remainder}.");
+    }
+}   

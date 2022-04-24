3834eb107cd26dd8a0b5e6c8c6931e2c5514c630
Puffkyoin <103963648+Puffkyoin@users.noreply.github.com>
2022-04-24 15:58:01 +0300

    Update task6.cs

--- task6.cs
+++ task6.cs
@@ -7,7 +7,7 @@ class Program
         int photosInAlbum = 52;
         int photosInRow = 3;
         int rowsFilled = photosInAlbum / photosInRow;
-        int remainder = photosInAlbum % photosInRow;
-        Console.WriteLine($" Заполнено {rowsFilled} строк.\n Остаток фото равен {remainder}.");
+        int remainderPhotos = photosInAlbum % photosInRow;
+        Console.WriteLine($" Заполнено {rowsFilled} строк.\n Остаток фото равен {remainderPhotos}.");
     }
 }   

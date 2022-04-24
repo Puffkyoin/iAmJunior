31351e9feeeda324755ce021e0060d4a95066b46
Puffkyoin <103963648+Puffkyoin@users.noreply.github.com>
2022-04-24 16:05:48 +0300

    Update task7.cs

--- task7.cs
+++ task7.cs
@@ -6,10 +6,10 @@ class Program
     {
         Console.WriteLine("Введите ваш баланс золота");
         int gold = Convert.ToInt32(Console.ReadLine());
-        int exchangeRate = 100;
+        int exchangeRateGoldInCristal = 100;
         Console.WriteLine($"Добрвй день. Какое количество кристалов вы хотите обменять сегодня? На данный момент курс составляет 1 к {exchangeRate}");
-        int Cristal = Convert.ToInt32(Console.ReadLine());
-        gold = gold - Cristal * exchangeRate;
-        Console.WriteLine($" Огромное спасибо за покупку. Приходите ещё!\n Инвентарь:\n Золото = {gold}\n Кристаллы = {Cristal}");
+        int cristals = Convert.ToInt32(Console.ReadLine());
+        gold -= cristals * exchangeRateGoldInCristal;
+        Console.WriteLine($" Огромное спасибо за покупку. Приходите ещё!\n Инвентарь:\n Золото = {gold}\n Кристаллы = {cristals}");
     }
 }   
